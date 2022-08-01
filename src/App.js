9e5c1c62ea70b0174c8f402db2d078251b37845a
yazawayuma1203 <110238678+yazawayuma1203@users.noreply.github.com>
2022-08-01 23:41:51 +0900

    useCallback, ueMemo useCallbackを用いての関数、変数のmemo化

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
@@ -12,13 +12,18 @@ export default function App() {
     setOpen(!open);
   };
 
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  const temp = useMemo(() => 1+3, []);
+  console.log(temp)
+
   return (
     <div className="App">
       <input value={text} onChange={onChangeText} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
